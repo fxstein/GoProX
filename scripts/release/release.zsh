3e88b372c8a5124b2283ea7d4c574644c7c93062
fxstein <773967+fxstein@users.noreply.github.com>
2025-06-22 18:28:05 +0200

    release: revert unsupported --run-name flag in workflow trigger (refs #20)

--- scripts/release/release.zsh
+++ scripts/release/release.zsh
@@ -128,8 +128,7 @@ trigger_workflow() {
     gh workflow run release-automation.yml \
         -f version="$version" \
         -f prev_version="$prev_version" \
-        -f dry_run="$dry_run" \
-        --run-name "Release v$version"
+        -f dry_run="$dry_run"
     
     if [[ $? -eq 0 ]]; then
         print_success "Workflow triggered successfully!"
